2d68d6b2f1cfa8ec954ca44584014743a8b4d186
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:14:39 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type Intersection<T> = any
 
 
 /* Write your code above */
@@ -11,9 +11,10 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Intersection<[[1, 2], [2, 3], [2, 2]]>, 2>>,
+  Expect<Equal<Intersection<[[1, 2, 3], [2, 3, 4], [2, 2, 3]]>, 2 | 3>>,
+  Expect<Equal<Intersection<[[1, 2], [3, 4], [5, 6]]>, never>>,
+  Expect<Equal<Intersection<[[1, 2, 3], [2, 3, 4], 3]>, 3>>,
+  Expect<Equal<Intersection<[[1, 2, 3], 2 | 3 | 4, 2 | 3]>, 2 | 3>>,
+  Expect<Equal<Intersection<[[1, 2, 3], 2, 3]>, never>>,
 ]
