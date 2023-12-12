1775493f02002b3e1c93c4e61d397b8e1e11d3b7
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:32:32 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,12 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+enum Comparison {
+  Greater,
+  Equal,
+  Lower,
+}
+
+type Comparator<A extends number, B extends number> = any
 
 
 /* Write your code above */
@@ -11,9 +17,36 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Comparator<5, 5>, Comparison.Equal>>,
+  Expect<Equal<Comparator<5, 6>, Comparison.Lower>>,
+  Expect<Equal<Comparator<5, 8>, Comparison.Lower>>,
+  Expect<Equal<Comparator<5, 0>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-5, 0>, Comparison.Lower>>,
+  Expect<Equal<Comparator<0, 0>, Comparison.Equal>>,
+  Expect<Equal<Comparator<0, -5>, Comparison.Greater>>,
+  Expect<Equal<Comparator<5, -3>, Comparison.Greater>>,
+  Expect<Equal<Comparator<5, -7>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-5, -7>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-5, -3>, Comparison.Lower>>,
+  Expect<Equal<Comparator<-25, -30>, Comparison.Greater>>,
+  Expect<Equal<Comparator<15, -23>, Comparison.Greater>>,
+  Expect<Equal<Comparator<40, 37>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-36, 36>, Comparison.Lower>>,
+  Expect<Equal<Comparator<27, 27>, Comparison.Equal>>,
+  Expect<Equal<Comparator<-38, -38>, Comparison.Equal>>,
+
+  Expect<Equal<Comparator<1, 100>, Comparison.Lower>>,
+  Expect<Equal<Comparator<100, 1>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-100, 1>, Comparison.Lower>>,
+  Expect<Equal<Comparator<1, -100>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-100, -1>, Comparison.Lower>>,
+  Expect<Equal<Comparator<-1, -100>, Comparison.Greater>>,
+
+  // Extra tests if you like to challenge yourself!
+  Expect<Equal<Comparator<9007199254740992, 9007199254740992>, Comparison.Equal>>,
+  Expect<Equal<Comparator<-9007199254740992, -9007199254740992>, Comparison.Equal>>,
+  Expect<Equal<Comparator<9007199254740991, 9007199254740992>, Comparison.Lower>>,
+  Expect<Equal<Comparator<9007199254740992, 9007199254740991>, Comparison.Greater>>,
+  Expect<Equal<Comparator<-9007199254740992, -9007199254740991>, Comparison.Lower>>,
+  Expect<Equal<Comparator<-9007199254740991, -9007199254740992>, Comparison.Greater>>,
 ]
