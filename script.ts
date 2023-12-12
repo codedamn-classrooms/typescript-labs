12877553f4339e89c97fc9111b7ebd06ca6c2d3a
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:21:22 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type IsRequiredKey<T, K extends keyof T> = any
 
 
 /* Write your code above */
@@ -11,9 +11,9 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'a'>, true>>,
+  Expect<Equal<IsRequiredKey<{ a: undefined; b: string }, 'a'>, true>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'b'>, false>>,
+  Expect<Equal<IsRequiredKey<{ a: number; b?: string }, 'b' | 'a'>, false>>,
+  Expect<Equal<IsRequiredKey<{ a: undefined; b: undefined }, 'b' | 'a'>, true>>,
 ]
