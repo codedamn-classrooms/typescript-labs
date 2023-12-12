78ddcb09b4c52e1bf7b1d40bfa92f33738c6ba22
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:07:28 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,7 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+// 实现 Combs
+type Combs<T extends any[]> = any
 
 
 /* Write your code above */
@@ -10,10 +11,9 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type ModifierKeys = ['cmd', 'ctrl', 'opt', 'fn']
+type CaseTypeOne = 'cmd ctrl' | 'cmd opt' | 'cmd fn' | 'ctrl opt' | 'ctrl fn' | 'opt fn'
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Combs<ModifierKeys>, CaseTypeOne>>,
 ]
