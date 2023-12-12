e3cdf03d3ca97632e63cb8385e1beace20279c70
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:08:48 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type ToPrimitive = any
 
 
 /* Write your code above */
@@ -10,10 +10,32 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type PersonInfo = {
+  name: 'Tom'
+  age: 30
+  married: false
+  addr: {
+    home: '123456'
+    phone: '13111111111'
+  }
+  hobbies: ['sing', 'dance']
+  readonlyArr: readonly ['test']
+  fn: () => any
+}
+
+type ExpectedResult = {
+  name: string
+  age: number
+  married: boolean
+  addr: {
+    home: string
+    phone: string
+  }
+  hobbies: [string, string]
+  readonlyArr: readonly [string]
+  fn: Function
+}
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<ToPrimitive<PersonInfo>, ExpectedResult>>,
 ]
