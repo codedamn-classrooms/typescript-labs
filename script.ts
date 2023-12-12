82ef41759d0d3582b6433db8f1ecc276c7f939b0
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:35:56 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type MyCapitalize<S extends string> = any
 
 
 /* Write your code above */
@@ -11,9 +11,34 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<MyCapitalize<'foobar'>, 'Foobar'>>,
+  Expect<Equal<MyCapitalize<'FOOBAR'>, 'FOOBAR'>>,
+  Expect<Equal<MyCapitalize<'foo bar'>, 'Foo bar'>>,
+  Expect<Equal<MyCapitalize<''>, ''>>,
+  Expect<Equal<MyCapitalize<'a'>, 'A'>>,
+  Expect<Equal<MyCapitalize<'b'>, 'B'>>,
+  Expect<Equal<MyCapitalize<'c'>, 'C'>>,
+  Expect<Equal<MyCapitalize<'d'>, 'D'>>,
+  Expect<Equal<MyCapitalize<'e'>, 'E'>>,
+  Expect<Equal<MyCapitalize<'f'>, 'F'>>,
+  Expect<Equal<MyCapitalize<'g'>, 'G'>>,
+  Expect<Equal<MyCapitalize<'h'>, 'H'>>,
+  Expect<Equal<MyCapitalize<'i'>, 'I'>>,
+  Expect<Equal<MyCapitalize<'j'>, 'J'>>,
+  Expect<Equal<MyCapitalize<'k'>, 'K'>>,
+  Expect<Equal<MyCapitalize<'l'>, 'L'>>,
+  Expect<Equal<MyCapitalize<'m'>, 'M'>>,
+  Expect<Equal<MyCapitalize<'n'>, 'N'>>,
+  Expect<Equal<MyCapitalize<'o'>, 'O'>>,
+  Expect<Equal<MyCapitalize<'p'>, 'P'>>,
+  Expect<Equal<MyCapitalize<'q'>, 'Q'>>,
+  Expect<Equal<MyCapitalize<'r'>, 'R'>>,
+  Expect<Equal<MyCapitalize<'s'>, 'S'>>,
+  Expect<Equal<MyCapitalize<'t'>, 'T'>>,
+  Expect<Equal<MyCapitalize<'u'>, 'U'>>,
+  Expect<Equal<MyCapitalize<'v'>, 'V'>>,
+  Expect<Equal<MyCapitalize<'w'>, 'W'>>,
+  Expect<Equal<MyCapitalize<'x'>, 'X'>>,
+  Expect<Equal<MyCapitalize<'y'>, 'Y'>>,
+  Expect<Equal<MyCapitalize<'z'>, 'Z'>>,
 ]
