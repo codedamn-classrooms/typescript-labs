759101b8025ae2371b5cb12b81e718e39a675fd9
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:27:34 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type UnionToTuple<T> = any
 
 
 /* Write your code above */
@@ -10,10 +10,18 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type ExtractValuesOfTuple<T extends any[]> = T[keyof T & number]
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<UnionToTuple<'a' | 'b'>['length'], 2>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a' | 'b'>>, 'a' | 'b'>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a'>>, 'a'>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<any>>, any>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<undefined | void | 1>>, void | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<any | 1>>, any | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<any | 1>>, any>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'d' | 'f' | 1 | never>>, 'f' | 'd' | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<[{ a: 1 }] | 1>>, [{ a: 1 }] | 1>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<never>>, never>>,
+  Expect<Equal<ExtractValuesOfTuple<UnionToTuple<'a' | 'b' | 'c' | 1 | 2 | 'd' | 'e' | 'f' | 'g'>>, 'f' | 'e' | 1 | 2 | 'g' | 'c' | 'd' | 'a' | 'b'>>,
 ]
