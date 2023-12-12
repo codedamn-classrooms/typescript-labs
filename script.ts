83cb0d4bc5d7706d7c805643a39e2b52531f4634
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:41:23 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+declare function SimpleVue(options: any): any
 
 
 /* Write your code above */
@@ -10,10 +10,38 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
-]
+SimpleVue({
+  data() {
+    // @ts-expect-error
+    this.firstname
+    // @ts-expect-error
+    this.getRandom()
+    // @ts-expect-error
+    this.data()
+
+    return {
+      firstname: 'Type',
+      lastname: 'Challenges',
+      amount: 10,
+    }
+  },
+  computed: {
+    fullname() {
+      return `${this.firstname} ${this.lastname}`
+    },
+  },
+  methods: {
+    getRandom() {
+      return Math.random()
+    },
+    hi() {
+      alert(this.amount)
+      alert(this.fullname.toLowerCase())
+      alert(this.getRandom())
+    },
+    test() {
+      const fullname = this.fullname
+      const cases: [Expect<Equal<typeof fullname, string>>] = [] as any
+    },
+  },
+})
