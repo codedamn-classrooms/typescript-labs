fe4b501b4ed976f8d7da0500e9ef1b6d75221346
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:30:38 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type Sum<A extends string | number | bigint, B extends string | number | bigint> = string
 
 
 /* Write your code above */
@@ -11,9 +11,13 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Sum<2, 3>, '5'>>,
+  Expect<Equal<Sum<'13', '21'>, '34'>>,
+  Expect<Equal<Sum<'328', 7>, '335'>>,
+  Expect<Equal<Sum<1_000_000_000_000n, '123'>, '1000000000123'>>,
+  Expect<Equal<Sum<9999, 1>, '10000'>>,
+  Expect<Equal<Sum<4325234, '39532'>, '4364766'>>,
+  Expect<Equal<Sum<728, 0>, '728'>>,
+  Expect<Equal<Sum<'0', 213>, '213'>>,
+  Expect<Equal<Sum<0, '0'>, '0'>>,
 ]
