e6e152601760475f6cc4319b7e8fdb885817dc61
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:10:10 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type CountElementNumberToObject<T> = any
 
 
 /* Write your code above */
@@ -9,11 +9,39 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
-
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<CountElementNumberToObject<[1, 2, 3, 4, 5]>, {
+    1: 1
+    2: 1
+    3: 1
+    4: 1
+    5: 1
+  }
+  >>,
+  Expect<Equal<CountElementNumberToObject<[1, 2, 3, 4, 5, [1, 2, 3]]>, {
+    1: 2
+    2: 2
+    3: 2
+    4: 1
+    5: 1
+  }>>,
+  Expect<Equal<CountElementNumberToObject<[1, 2, 3, 4, 5, [1, 2, 3, [4, 4, 1, 2]]]>, {
+    1: 3
+    2: 3
+    3: 2
+    4: 3
+    5: 1
+  }>>,
+  Expect<Equal<CountElementNumberToObject<[never]>, {}>>,
+  Expect<Equal<CountElementNumberToObject<['1', '2', '0']>, {
+    0: 1
+    1: 1
+    2: 1
+  }>>,
+  Expect<Equal<CountElementNumberToObject<['a', 'b', ['c', ['d']]]>, {
+    'a': 1
+    'b': 1
+    'c': 1
+    'd': 1
+  }>>,
 ]
