9f970a5064c8306275219a60ea7b95a01bec54f7
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:12:05 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type CheckRepeatedChars<T extends string> = any
 
 
 /* Write your code above */
@@ -9,11 +9,11 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<CheckRepeatedChars<'abc'>, false>>,
+  Expect<Equal<CheckRepeatedChars<'abb'>, true>>,
+  Expect<Equal<CheckRepeatedChars<'cbc'>, true>>,
+  Expect<Equal<CheckRepeatedChars<''>, false>>,
 ]
