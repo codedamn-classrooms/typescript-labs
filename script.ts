fe06cc0438d18085b80e857f321e5804d959d2f3
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:04:17 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type DeepOmit = any
 
 
 /* Write your code above */
@@ -10,10 +10,18 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type obj = {
+  person: {
+    name: string
+    age: {
+      value: number
+    }
+  }
+}
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<DeepOmit<obj, 'person'>, {}>>,
+  Expect<Equal<DeepOmit<obj, 'person.name'>, { person: { age: { value: number } } }>>,
+  Expect<Equal<DeepOmit<obj, 'name'>, obj>>,
+  Expect<Equal<DeepOmit<obj, 'person.age.value'>, { person: { name: string; age: {} } }>>,
 ]
