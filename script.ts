97826f37d6166ed5a37a7c6acd929aac12ae34af
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:11:52 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type ValidDate<T extends string> = any
 
 
 /* Write your code above */
@@ -11,9 +11,14 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<ValidDate<'0102'>, true>>,
+  Expect<Equal<ValidDate<'0131'>, true>>,
+  Expect<Equal<ValidDate<'1231'>, true>>,
+  Expect<Equal<ValidDate<'0229'>, false>>,
+  Expect<Equal<ValidDate<'0100'>, false>>,
+  Expect<Equal<ValidDate<'0132'>, false>>,
+  Expect<Equal<ValidDate<'1301'>, false>>,
+  Expect<Equal<ValidDate<'0123'>, true>>,
+  Expect<Equal<ValidDate<'01234'>, false>>,
+  Expect<Equal<ValidDate<''>, false>>,
 ]
