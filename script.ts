e59fa9fb2cd473641f6abd79063f64a520d56340
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:18:29 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type IsPalindrome<T> = any
 
 
 /* Write your code above */
@@ -11,9 +11,12 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<IsPalindrome<'abc'>, false>>,
+  Expect<Equal<IsPalindrome<'b'>, true>>,
+  Expect<Equal<IsPalindrome<'abca'>, false>>,
+  Expect<Equal<IsPalindrome<'abba'>, true>>,
+  Expect<Equal<IsPalindrome<'abcba'>, true>>,
+  Expect<Equal<IsPalindrome<121>, true>>,
+  Expect<Equal<IsPalindrome<2332>, true>>,
+  Expect<Equal<IsPalindrome<19260817>, false>>,
 ]
