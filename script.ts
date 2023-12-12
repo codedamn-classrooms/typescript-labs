0b4a79300a769b8adfff351c1523400ceb0330b7
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:08:36 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type DeepMutable = any
 
 
 /* Write your code above */
@@ -10,10 +10,76 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+interface Test1 {
+  readonly title: string
+  readonly description: string
+  readonly completed: boolean
+  readonly meta: {
+    readonly author: string
+  }
+}
+type Test2 = {
+  readonly a: () => 1
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 's'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+}
+interface DeepMutableTest1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
+
+type DeepMutableTest2 = {
+  a: () => 1
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 's'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+}
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<DeepMutable<Test1>, DeepMutableTest1>>,
+  Expect<Equal<DeepMutable<Test2>, DeepMutableTest2>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  DeepMutable<'string'>,
+  // @ts-expect-error
+  DeepMutable<0>,
 ]
