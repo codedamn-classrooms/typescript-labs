3176b850eac78963d93df8bc9f4a758fea7b5f78
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:07:55 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type SnakeCase<T> = any
 
 
 /* Write your code above */
@@ -9,11 +9,11 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<SnakeCase<'hello'>, 'hello'>>,
+  Expect<Equal<SnakeCase<'userName'>, 'user_name'>>,
+  Expect<Equal<SnakeCase<'getElementById'>, 'get_element_by_id'>>,
+  Expect<Equal<SnakeCase<'getElementById' | 'getElementByClassNames'>, 'get_element_by_id' | 'get_element_by_class_names'>>,
 ]
