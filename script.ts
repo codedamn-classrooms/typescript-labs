8844035c7ef2f4b4f0ff7897e0be4009d340cbd6
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:11:26 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type FirstUniqueCharIndex<T extends string> = any
 
 
 /* Write your code above */
@@ -11,9 +11,9 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<FirstUniqueCharIndex<'leetcode'>, 0>>,
+  Expect<Equal<FirstUniqueCharIndex<'loveleetcode'>, 2>>,
+  Expect<Equal<FirstUniqueCharIndex<'aabb'>, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<''>, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<'aaa'>, -1>>,
 ]
