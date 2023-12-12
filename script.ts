08a1efb19b28c2a13c73a38d8ca57e96d60693d9
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:33:53 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type AppendArgument<Fn, A> = any
 
 
 /* Write your code above */
@@ -10,10 +10,15 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>
+type Result1 = (a: number, b: string, x: boolean) => number
+
+type Case2 = AppendArgument<() => void, undefined>
+type Result2 = (x: undefined) => void
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Case1, Result1>>,
+  Expect<Equal<Case2, Result2>>,
+  // @ts-expect-error
+  AppendArgument<unknown, undefined>,
 ]
