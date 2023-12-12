68747e9f068a873983afa1edf7c168914ae58963
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:30:22 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type Multiply<A extends string | number | bigint, B extends string | number | bigint> = string
 
 
 /* Write your code above */
@@ -11,9 +11,20 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Multiply<2, 3>, '6'>>,
+  Expect<Equal<Multiply<3, '5'>, '15'>>,
+  Expect<Equal<Multiply<'4', 10>, '40'>>,
+  Expect<Equal<Multiply<0, 16>, '0'>>,
+  Expect<Equal<Multiply<'13', '21'>, '273'>>,
+  Expect<Equal<Multiply<'43423', 321543n>, '13962361689'>>,
+  Expect<Equal<Multiply<9999, 1>, '9999'>>,
+  Expect<Equal<Multiply<4325234, '39532'>, '170985150488'>>,
+  Expect<Equal<Multiply<100_000n, '1'>, '100000'>>,
+  Expect<Equal<Multiply<259, 9125385>, '2363474715'>>,
+  Expect<Equal<Multiply<9, 99>, '891'>>,
+  Expect<Equal<Multiply<315, '100'>, '31500'>>,
+  Expect<Equal<Multiply<11n, 13n>, '143'>>,
+  Expect<Equal<Multiply<728, 0>, '0'>>,
+  Expect<Equal<Multiply<'0', 213>, '0'>>,
+  Expect<Equal<Multiply<0, '0'>, '0'>>,
 ]
