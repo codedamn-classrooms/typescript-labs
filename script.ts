a0c3a705d777d456a11ec8dddf041e2b85b23ff6
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:18:42 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,11 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+interface TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+}
+type InorderTraversal<T extends TreeNode | null> = any
 
 
 /* Write your code above */
@@ -10,10 +15,50 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+const tree1 = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+} as const
+
+const tree2 = {
+  val: 1,
+  left: null,
+  right: null,
+} as const
+
+const tree3 = {
+  val: 1,
+  left: {
+    val: 2,
+    left: null,
+    right: null,
+  },
+  right: null,
+} as const
+
+const tree4 = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: null,
+    right: null,
+  },
+} as const
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<InorderTraversal<null>, []>>,
+  Expect<Equal<InorderTraversal<typeof tree1>, [1, 3, 2]>>,
+  Expect<Equal<InorderTraversal<typeof tree2>, [1]>>,
+  Expect<Equal<InorderTraversal<typeof tree3>, [2, 1]>>,
+  Expect<Equal<InorderTraversal<typeof tree4>, [1, 2]>>,
 ]
