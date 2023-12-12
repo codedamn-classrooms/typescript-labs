3941e7bda6bd0eff9c6ccd4a19272cbcc114a5a2
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:10:51 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type CapitalizeNestObjectKeys<T> = any
 
 
 /* Write your code above */
@@ -9,11 +9,20 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type foo = {
+  foo: string
+  bars: [{ foo: string }]
+}
+
+type Foo = {
+  Foo: string
+  Bars: [{
+    Foo: string
+  }]
+}
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Foo, CapitalizeNestObjectKeys<foo>>>,
 ]
