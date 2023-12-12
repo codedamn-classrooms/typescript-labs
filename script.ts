ad00367a5938f72008cae995336bd8f9d21a2db0
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:05:22 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type MergeAll<XS> = any
 
 
 /* Write your code above */
@@ -11,9 +11,25 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<MergeAll<[]>, {} >>,
+  Expect<Equal<MergeAll<[{ a: 1 }]>, { a: 1 }>>,
+  Expect<Equal<
+    MergeAll<[{ a: string }, { a: string }]>,
+    { a: string }>
+  >,
+  Expect<Equal<
+    MergeAll<[{ }, { a: string }]>,
+    { a: string }>
+  >,
+  Expect<Equal<
+    MergeAll<[{ a: 1 }, { c: 2 }]>,
+    { a: 1; c: 2 }>
+  >,
+  Expect<Equal<
+    MergeAll<[{ a: 1; b: 2 }, { a: 2 }, { c: 3 }]>,
+    { a: 1 | 2; b: 2; c: 3 }>
+  >,
+  Expect<Equal<MergeAll<[{ a: 1 }, { a: number }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: number }, { a: 1 }]>, { a: number }>>,
+  Expect<Equal<MergeAll<[{ a: 1 | 2 }, { a: 1 | 3 }]>, { a: 1 | 2 | 3 }>>,
 ]
