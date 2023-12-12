16d8fc9512f6a5b5b9c0c8aceb0c814e8e2984d2
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:35:17 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type CamelCase<S extends string> = any
 
 
 /* Write your code above */
@@ -11,9 +11,17 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'FOOBAR'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo__bar'>, 'foo_Bar'>>,
+  Expect<Equal<CamelCase<'foo_$bar'>, 'foo_$bar'>>,
+  Expect<Equal<CamelCase<'foo_bar_'>, 'fooBar_'>>,
+  Expect<Equal<CamelCase<'foo_bar__'>, 'fooBar__'>>,
+  Expect<Equal<CamelCase<'foo_bar_$'>, 'fooBar_$'>>,
+  Expect<Equal<CamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
+  Expect<Equal<CamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>,
 ]
