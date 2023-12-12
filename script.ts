9dad13b2f10624fbc5c14b49afe9ba6b10d0bb26
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:31:10 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+declare function DynamicParamsCurrying(fn: any): any
 
 
 /* Write your code above */
@@ -10,10 +10,37 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+const curried1 = DynamicParamsCurrying((a: string, b: number, c: boolean) => true)
+const curried2 = DynamicParamsCurrying((a: string, b: number, c: boolean, d: boolean, e: boolean, f: string, g: boolean) => true)
+
+const curried1Return1 = curried1('123')(123)(true)
+const curried1Return2 = curried1('123', 123)(false)
+const curried1Return3 = curried1('123', 123, true)
+
+const curried2Return1 = curried2('123')(123)(true)(false)(true)('123')(false)
+const curried2Return2 = curried2('123', 123)(true, false)(true, '123')(false)
+const curried2Return3 = curried2('123', 123)(true)(false)(true, '123', false)
+const curried2Return4 = curried2('123', 123, true)(false, true, '123')(false)
+const curried2Return5 = curried2('123', 123, true)(false)(true)('123')(false)
+const curried2Return6 = curried2('123', 123, true, false)(true, '123', false)
+const curried2Return7 = curried2('123', 123, true, false, true)('123', false)
+const curried2Return8 = curried2('123', 123, true, false, true)('123')(false)
+const curried2Return9 = curried2('123', 123, true, false, true, '123')(false)
+const curried2Return10 = curried2('123', 123, true, false, true, '123', false)
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal< typeof curried1Return1, boolean>>,
+  Expect<Equal< typeof curried1Return2, boolean>>,
+  Expect<Equal< typeof curried1Return3, boolean>>,
+
+  Expect<Equal< typeof curried2Return1, boolean>>,
+  Expect<Equal< typeof curried2Return2, boolean>>,
+  Expect<Equal< typeof curried2Return3, boolean>>,
+  Expect<Equal< typeof curried2Return4, boolean>>,
+  Expect<Equal< typeof curried2Return5, boolean>>,
+  Expect<Equal< typeof curried2Return6, boolean>>,
+  Expect<Equal< typeof curried2Return7, boolean>>,
+  Expect<Equal< typeof curried2Return8, boolean>>,
+  Expect<Equal< typeof curried2Return9, boolean>>,
+  Expect<Equal< typeof curried2Return10, boolean>>,
 ]
