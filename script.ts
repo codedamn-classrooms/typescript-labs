02f84db242fd8602d153ed8854d8e9ddad15dd5d
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:15:33 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type MutableKeys<T> = any
 
 
 /* Write your code above */
@@ -11,9 +11,8 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<MutableKeys<{ a: number; readonly b: string }>, 'a'>>,
+  Expect<Equal<MutableKeys<{ a: undefined; readonly b: undefined }>, 'a'>>,
+  Expect<Equal<MutableKeys<{ a: undefined; readonly b?: undefined; c: string; d: null }>, 'a' | 'c' | 'd'>>,
+  Expect<Equal<MutableKeys<{}>, never>>,
 ]
