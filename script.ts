1f53a9d4256421a18a0f420b7ea6702bb77eaa48
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:31:36 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type FilterOut<T extends any[], F> = any
 
 
 /* Write your code above */
@@ -11,9 +11,10 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<FilterOut<[], never>, []>>,
+  Expect<Equal<FilterOut<[never], never>, []>>,
+  Expect<Equal<FilterOut<['a', never], never>, ['a']>>,
+  Expect<Equal<FilterOut<[1, never, 'a'], never>, [1, 'a']>>,
+  Expect<Equal<FilterOut<[never, 1, 'a', undefined, false, null], never | null | undefined>, [1, 'a', false]>>,
+  Expect<Equal<FilterOut<[number | null | undefined, never], never | null | undefined>, [number | null | undefined]>>,
 ]
