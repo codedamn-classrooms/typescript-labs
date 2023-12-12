7af57b7790a38c5ec9d350c3eb4beca1a1f90d07
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 17:46:10 +0530

    Add files via script

---
+++ script.ts
@@ -0,0 +1,19 @@
+/* Write your code below */
+
+type BitwiseXOR<S1 extends string, S2 extends string> = any
+
+
+/* Write your code above */
+
+
+
+/* There should be no error in the test cases below */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
+  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
+  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
+  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
+  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+]
