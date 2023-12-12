ab02841e5f925ba828a62e9181014a8f41607b46
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:08:07 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type Filter<T extends any[], P> = []
 
 
 /* Write your code above */
@@ -10,10 +10,10 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Falsy = false | 0 | '' | null | undefined
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<Filter<[0, 1, 2], 2>, [2]>>,
+  Expect<Equal<Filter<[0, 1, 2], 0 | 1>, [0, 1]>>,
+  Expect<Equal<Filter<[0, 1, 2], Falsy>, [0]>>,
 ]
