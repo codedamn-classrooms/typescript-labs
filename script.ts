bb2b84ad86e5cdb2e348be389d87e679efaa37cd
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:10:25 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type FindEles<T extends any[]> = any
 
 
 /* Write your code above */
@@ -9,11 +9,10 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 6]>, [1, 4, 5]>>,
+  Expect<Equal<FindEles<[2, 2, 3, 3, 6, 6, 6]>, []>>,
+  Expect<Equal<FindEles<[1, 2, 3]>, [1, 2, 3]>>,
 ]
