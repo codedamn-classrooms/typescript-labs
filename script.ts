58441893a1dee34282a2262f20129b420cec9738
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:31:51 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type ToNumber<S extends string> = any
 
 
 /* Write your code above */
@@ -11,9 +11,9 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<ToNumber<'0'>, 0>>,
+  Expect<Equal<ToNumber<'5'>, 5>>,
+  Expect<Equal<ToNumber<'12'>, 12>>,
+  Expect<Equal<ToNumber<'27'>, 27>>,
+  Expect<Equal<ToNumber<'18@7_$%'>, never>>,
 ]
