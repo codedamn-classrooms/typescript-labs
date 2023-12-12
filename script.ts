b5e5c779708148f8f08bd68bd83c46945796ddd5
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:41:35 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type GetReadonlyKeys<T> = any
 
 
 /* Write your code above */
@@ -11,9 +11,18 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<'title', GetReadonlyKeys<Todo1>>>,
+  Expect<Equal<'title' | 'description', GetReadonlyKeys<Todo2>>>,
 ]
+
+interface Todo1 {
+  readonly title: string
+  description: string
+  completed: boolean
+}
+
+interface Todo2 {
+  readonly title: string
+  readonly description: string
+  completed?: boolean
+}
