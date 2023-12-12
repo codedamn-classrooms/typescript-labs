3bafc523b0294086ccfcb9fca70f63c29aa5a442
Mehul Mohan <mehulmpt@gmail.com>
2023-12-12 18:21:50 +0530

    Add files via script

--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
 /* Write your code below */
 
-type BitwiseXOR<S1 extends string, S2 extends string> = any
+type ClassPublicKeys = any
 
 
 /* Write your code above */
@@ -10,10 +10,21 @@ type BitwiseXOR<S1 extends string, S2 extends string> = any
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
 
+class A {
+  public str: string
+  protected num: number
+  private bool: boolean
+  constructor() {
+    this.str = 'naive'
+    this.num = 19260917
+    this.bool = true
+  }
+
+  getNum() {
+    return Math.random()
+  }
+}
+
 type cases = [
-  Expect<Equal<BitwiseXOR<'0', '1'>, '1'>>,
-  Expect<Equal<BitwiseXOR<'1', '1'>, '0'>>,
-  Expect<Equal<BitwiseXOR<'10', '1'>, '11'>>,
-  Expect<Equal<BitwiseXOR<'110', '1'>, '111'>>,
-  Expect<Equal<BitwiseXOR<'101', '11'>, '110'>>,
+  Expect<Equal<ClassPublicKeys<A>, 'str' | 'getNum'>>,
 ]
