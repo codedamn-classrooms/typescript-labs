1f10fc8713f6897f8ccd061b68248b1539662838
Pranav <pranav.techiegeek@gmail.com>
2023-12-14 15:25:01 +0530

    ready

--- script.ts
+++ script.ts
@@ -16,7 +16,7 @@ type cases = [
   Expect<Equal<Trunc<1.234>, '1'>>,
   Expect<Equal<Trunc<12.345>, '12'>>,
   Expect<Equal<Trunc<-5.1>, '-5'>>,
-  Expect<Equal<Trunc<'.3'>, '0'>>,
+  Expect<Equal<Trunc<'0.3'>, '0'>>,
   Expect<Equal<Trunc<'1.234'>, '1'>>,
   Expect<Equal<Trunc<'-10.234'>, '-10'>>,
   Expect<Equal<Trunc<10>, '10'>>,
