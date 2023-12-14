65fca9cab88e69a6a5a2b39ddd43354b7154cc99
Pranav <pranav.techiegeek@gmail.com>
2023-12-14 15:04:42 +0530

    ready

--- script.ts
+++ script.ts
@@ -1,21 +1,6 @@
 /* Write your code below */
 
-type Pure<T> = {
-  [P in keyof T]: T[P] extends object ? Pure<T[P]> : T[P]
-}
-
-type SetProperty<T, K extends PropertyKey, V> = {
-  [P in (keyof T) | K]: P extends K ? V : P extends keyof T ? T[P] : never
-}
-
-type Token = any
-type ParseResult<T, K extends Token[]> = [T, K]
-type Tokenize<T extends string, S extends Token[] = []> = Token[]
-type ParseLiteral<T extends Token[]> = ParseResult<any, T>
-
-type Parse<T extends string> = Pure<ParseLiteral<Tokenize<T>>[0]>
-
-
+type Parse<T extends string> = {}
 /* Write your code above */
 
 
@@ -37,17 +22,17 @@ type cases = [
       }
     `>
   ), (
-    {
-      nil: null
-      c: [true, false, 'hello', {
-        a: 'b'
+      {
+        nil: null
+        c: [true, false, 'hello', {
+          a: 'b'
+          b: false
+        }]
         b: false
-      }]
-      b: false
-      a: 'b'
-    }
+        a: 'b'
+      }
 
-  )>>,
+    )>>,
   Expect<Equal<Parse<'{}'>, {}>>,
 
   Expect<Equal<Parse<'[]'>, []>>,
@@ -57,21 +42,21 @@ type cases = [
   Expect<Equal<Parse<'true'>, true>>,
 
   Expect<Equal<
-  Parse<'["Hello", true, false, null]'>,
-  ['Hello', true, false, null]
+    Parse<'["Hello", true, false, null]'>,
+    ['Hello', true, false, null]
   >>,
 
   Expect<Equal<
-  (
-    Parse<`
+    (
+      Parse<`
       {
         "hello\\r\\n\\b\\f": "world"
       }`>
-  ), (
-    {
-      'hello\r\n\b\f': 'world'
-    }
-  )
+    ), (
+      {
+        'hello\r\n\b\f': 'world'
+      }
+    )
   >>,
 
   Expect<Equal<Parse<'{ 1: "world" }'>, never>>,
