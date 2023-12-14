a67853e9c465474b6721b494a343672bf78d0e3d
Pranav <pranav.techiegeek@gmail.com>
2023-12-14 19:08:26 +0530

    ready

--- script.ts
+++ script.ts
@@ -19,6 +19,3 @@ type cases = [
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
   Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
 ]
-
-// @ts-expect-error
-type error = Concat<null, undefined>
