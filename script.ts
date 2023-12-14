8e7561765b9c4c706ec4814b31449c87a9ab23c3
Pranav <pranav.techiegeek@gmail.com>
2023-12-14 14:41:16 +0530

    ready

--- script.ts
+++ script.ts
@@ -9,7 +9,6 @@ type FindEles<T extends any[]> = any
 
 /* There should be no error in the test cases below */
 import type { Equal, Expect } from '@type-challenges/utils'
-import { ExpectFalse, NotEqual } from '@type-challenges/utils'
 
 type cases = [
   Expect<Equal<FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 6]>, [1, 4, 5]>>,
